d0d7ac32752250499e866ffcb146d3ffa6d90267
tomkoset <tomkoset@umich.edu>
2020-04-15 11:30:49 -0400

    Update to new mothur analysis script and change cpus-per-task instead of ntasks-per_node

--- code/slurm/mothur_intial.sh
+++ code/slurm/mothur_intial.sh
@@ -13,8 +13,8 @@
 # For MPI, increase ntasks-per-node
 # For multithreading, increase cpus-per-task
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=12
-#SBATCH --cpus-per-task=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=10
 #SBATCH --mem-per-cpu=4GB
 #SBATCH --time=5:00:00
 
@@ -39,5 +39,4 @@
 #####################
 
 
-/nfs/turbo/schloss-lab/bin/mothur code/get_good_seqs.batch
-#/nfs/turbo/schloss-lab/bin/mothur code/get_shared_otus.batch
+/nfs/turbo/schloss-lab/bin/mothur code/get_good_seqs_shared_otus.batch
