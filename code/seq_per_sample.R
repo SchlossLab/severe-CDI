22c426eb7864dc10bd4d6c34b44c5725b6b036df
tomkoset <tomkoset@umich.edu>
2020-04-19 15:50:49 -0400

    Add plot to help determine sequences per sample cutoff

--- code/seq_per_sample.R
+++ code/seq_per_sample.R
@@ -1,3 +1,4 @@
+library(tidyverse)
 # mothur "#count.groups with data/mothur/cdi.trim.contigs.good.unique.good.filter.unique.precluster.denovo.vsearch.pick.pick.pick.count_table as input
 # plot number of samples that have less than 1000, 1500, 5000, 10000 samples to determine the number of samples we want to resequence
 
@@ -7,7 +8,7 @@ data <- read_tsv("data/mothur/cdi.trim.contigs.good.unique.good.filter.unique.pr
   filter(!str_detect(sample, "PBS")) #Remove PBS aliquot samples
 
 #Total # of samples
-data %>% count(sample)
+total <- data %>% count(sample)
 
 data %>% ggplot(aes(x=nseqs)) + geom_histogram()
 data %>% ggplot(aes(x=nseqs)) + geom_histogram() + scale_x_log10()
@@ -16,17 +17,34 @@ data %>% select(sample, nseqs) %>% filter(nseqs < 1000) %>% count(sample)
 
 
 #If I rarefy to 1000:
-data %>% filter(nseqs < 1000) %>% arrange(sample) %>% count(sample)
+total_1000 <- data %>% filter(nseqs < 1000) %>% arrange(sample) %>% count(sample)
+n_1000 = total- total_1000 
 #I'll lose  samples.
 
 #If I rarefy to 1500:
-data %>% filter(nseqs < 1500) %>% arrange(sample) %>% count(sample)
+total_1500 <- data %>% filter(nseqs < 1500) %>% arrange(sample) %>% count(sample)
+n_1500 = total- total_1500 
 #I'll lose  samples.
 
 #If I rarefy to 5000:
-data %>% filter(nseqs < 5000) %>% arrange(sample) %>% count(sample) 
+total_5000 <- data %>% filter(nseqs < 5000) %>% arrange(sample) %>% count(sample) 
+n_5000 = total- total_5000 
 #I'll lose  samples.
 
 #If I rarefy to 10000:
-data %>% filter(nseqs < 10000) %>% arrange(sample) %>% count(sample)
+total_10000 <- data %>% filter(nseqs < 10000) %>% arrange(sample) %>% count(sample)
+n_10000 = total- total_10000 
 #I'll lose  samples.
+
+# Data frame of samples lost depending on sequences per sample cutoff
+data_lost <- tibble("nseq" = c(1000, 1500, 5000, 10000), "n_samples_lost" = c(n_1000, n_1500, n_5000, n_10000))
+
+#Plot visualizing number of samples to be resequenced depending on the cutoff chosen for number of sequences per sample chosen
+data_lost_plot <- data_lost %>% 
+  ggplot(aes(x=nseq, y= n_samples_lost))+
+  geom_bar(stat = "identity")+
+  labs(title=NULL,
+     x="Minimum Number of Sequences Per Sample",
+     y= "Number of Samples to Resequence")+
+  theme_classic()+
+  ggsave("exploratory/notebook/resequencing_cutoff.pdf")
