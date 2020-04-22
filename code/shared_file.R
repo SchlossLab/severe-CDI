5e1091514cee2063e2739576dc5d3f031cdfa91b
tomkoset <tomkoset@umich.edu>
2020-04-22 10:41:57 -0400

    Code to read in shared file and correct 1 sample ID and drop 1 duplicate sample

---
+++ code/shared_file.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+
+#If we choose sequencing cutoff of 5000 sequences per sample, there will be 385 samples that need to be resequenced.
+#This includes 7 samples that are currently missing from shared file, but the data for them are likely on the MiSeq.
+#Sample IDs for the 7 missing samples (plate_20, column 12) # KR01434, KR01457, KR01469, KR01481, KR01493, KR01505, KR01445
+#Lucas will check once labs are back open and then I will check to see how many sequences per sample there are for the 7 samples.
+
+#Load in shared file
+#A few sample names need to be corrected so that they will match their corresponding CDIS_Sample_ID
+shared <- read_tsv("data/mothur/cdi.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.pick.opti_mcc.shared") %>% 
+  select(-label, -numOtus) %>% 
+  mutate(CDIS_Sample_ID= as.character(Group)) %>% 
+  mutate(CDIS_Sample_ID = replace(CDIS_Sample_ID, CDIS_Sample_ID == "KR00442M1", "KR00442")) %>% #This is sample KR00442, check pattern matching for when I renamed files before running them through mothur
+  filter(CDIS_Sample_ID != "KR01437M12") #Drop KR01437M12. This well was left empty (KR01437_M1_2) as it was a duplicate of KR01437. I hadn't realized that there was a duplicate of one 
+                                         #sample in the list of aliquots I was working from until we were pulling samples from the -80.
+
