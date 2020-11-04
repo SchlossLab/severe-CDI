bac62944ad95dec2b3a7ab3954d89628332f1393
tomkoset <tomkoset@umich.edu>
2020-11-04 14:17:11 -0500

    Update shared file notes after final run of samples through mothur.

--- code/shared_file.R
+++ code/shared_file.R
@@ -1,12 +1,9 @@
 library(tidyverse)
 
-#If we choose sequencing cutoff of 5000 sequences per sample, there will be 385 samples that need to be resequenced.
-#This includes 7 samples that are currently missing from shared file, but the data for them are likely on the MiSeq.
-#Sample IDs for the 7 missing samples (plate_20, column 12) # KR01434, KR01457, KR01469, KR01481, KR01493, KR01505, KR01445
-#Lucas will check once labs are back open and then I will check to see how many sequences per sample there are for the 7 samples.
+#Load in shared file (has not been sub sampled)
+#11 samples have less than 5000 sequences per sample despite several attempts at resequencing. 
+#Includes 45 water controls and 3 PBS aliquots (all have < 5000 sequences and will be dropped after sub sampling to 5000 sequences)
 
-#Load in shared file
-#A few sample names need to be corrected so that they will match their corresponding CDIS_Sample_ID
 shared <- read_tsv("data/mothur/cdi.trim.contigs.good.unique.good.filter.unique.precluster.pick.pick.pick.opti_mcc.shared") %>% 
   select(-label, -numOtus) %>% 
   mutate(CDIS_Sample_ID= as.character(Group)) 
