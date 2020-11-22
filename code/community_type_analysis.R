a2aed55af0ddc2afef676cfefc13e1c717375872
tomkoset <tomkoset@umich.edu>
2020-11-22 18:47:26 -0500

    Work on recreating community type plot from 2014 paper

--- code/community_type_analysis.R
+++ code/community_type_analysis.R
@@ -54,7 +54,9 @@ save_plot(filename = "exploratory/notebook/cdi_community_type_laplace.png", lapl
 
 #Read in best cluster for each sample
 sample_best_cluster_fit <- read_tsv("data/process/cdi.subsample.genus.genus.dmm.mix.design", col_names=c("sample", "best_fitting_cluster")) %>% 
-  left_join(metadata, by = "sample")
+  left_join(metadata, by = "sample") %>% 
+  mutate(cluster=str_replace(best_fitting_cluster,"Partition_(\\d*)","\\1")) %>% #Add a column with just the cluster number
+  mutate(cluster= as.numeric(cluster)) #Transform cluster variable type from character to numeric
 
 best_cluster_group_plot <- sample_best_cluster_fit %>% 
   group_by(group) %>% 
@@ -67,7 +69,7 @@ best_cluster_group_plot <- sample_best_cluster_fit %>%
 
 #Figure out percent cases, nondiarrheal controls and diarrheal controls by cluster
 percent_group <- sample_best_cluster_fit %>% 
-  group_by(best_fitting_cluster, group) %>% 
+  group_by(cluster, group) %>% 
   summarize(group_cluster_total = n()) %>% 
   #Make a new variable % group per cluster based on total group numbers
   mutate(percent_group = case_when(group == "case" ~ (group_cluster_total/1517)*100,
@@ -75,33 +77,42 @@ percent_group <- sample_best_cluster_fit %>%
                                            group == "nondiarrheal_control" ~ (group_cluster_total/909)*100,
                                            TRUE ~ 0)) %>% #No samples should fall into this category
   ggplot()+
-  geom_tile(aes(x=best_fitting_cluster, y=group, fill=percent_group))+
+  geom_tile(aes(x=cluster, y=group, fill=percent_group))+
+  scale_y_discrete(label = c("Diarrheal Control", "Non-Diarrheal Control", "Case"))+
   theme_classic()+
-  scale_fill_distiller(palette = "YlGnBu", direction = 1) 
+#  scale_fill_viridis(name = "% Group") Alternative color scale
+  scale_fill_distiller(palette = "YlGnBu", direction = 1, name = "% Group") 
 
 percent_cluster <- sample_best_cluster_fit %>% 
-  group_by(best_fitting_cluster, group) %>% 
+  group_by(cluster, group) %>% 
   summarize(group_cluster_total = n()) %>%
   #Make a new variable % group per cluster based on total cluster numbers
-  mutate(percent_cluster = case_when(best_fitting_cluster == "Partition_1" ~ (group_cluster_total/221)*100,
-                                     best_fitting_cluster == "Partition_10" ~ (group_cluster_total/421)*100,
-                                     best_fitting_cluster == "Partition_11" ~ (group_cluster_total/464)*100,
-                                     best_fitting_cluster == "Partition_12" ~ (group_cluster_total/81)*100,
-                                     best_fitting_cluster == "Partition_2" ~ (group_cluster_total/367)*100,
-                                     best_fitting_cluster == "Partition_3" ~ (group_cluster_total/283)*100,
-                                     best_fitting_cluster == "Partition_4" ~ (group_cluster_total/504)*100,
-                                     best_fitting_cluster == "Partition_5" ~ (group_cluster_total/243)*100,
-                                     best_fitting_cluster == "Partition_6" ~ (group_cluster_total/163)*100,
-                                     best_fitting_cluster == "Partition_7" ~ (group_cluster_total/421)*100,
-                                     best_fitting_cluster == "Partition_8" ~ (group_cluster_total/280)*100,
-                                     best_fitting_cluster == "Partition_9" ~ (group_cluster_total/484)*100,
+  mutate(percent_cluster = case_when(cluster == "1" ~ (group_cluster_total/221)*100,
+                                     cluster == "10" ~ (group_cluster_total/421)*100,
+                                     cluster == "11" ~ (group_cluster_total/464)*100,
+                                     cluster == "12" ~ (group_cluster_total/81)*100,
+                                     cluster == "2" ~ (group_cluster_total/367)*100,
+                                     cluster == "3" ~ (group_cluster_total/283)*100,
+                                     cluster == "4" ~ (group_cluster_total/504)*100,
+                                     cluster == "5" ~ (group_cluster_total/243)*100,
+                                     cluster == "6" ~ (group_cluster_total/163)*100,
+                                     cluster == "7" ~ (group_cluster_total/421)*100,
+                                     cluster == "8" ~ (group_cluster_total/280)*100,
+                                     cluster == "9" ~ (group_cluster_total/484)*100,
                                      TRUE ~ 0)) %>% #No samples should fall into this category
   mutate(group = factor(group, levels = unique(as.factor(group)))) %>% #Transform group variable into factor variable
   mutate(group = fct_relevel(group, "diarrheal_control", "nondiarrheal_control", "case")) %>% #Specify the order of the groups
   ggplot()+
-  geom_tile(aes(x=best_fitting_cluster, y=group, fill=percent_cluster))+
+  geom_tile(aes(x=cluster, y=group, fill=percent_cluster))+
+  scale_x_continuous(breaks = c(1:12))+
+  scale_y_discrete(label = c("Diarrheal Control", "Non-Diarrheal Control", "Case"))+
   theme_classic()+
-  scale_fill_distiller(palette = "YlGnBu", direction = 1)
+#  scale_fill_viridis(name = "% Cluster") Alternate scale
+  scale_fill_distiller(palette = "YlGnBu", direction = 1, name = "% Cluster")+
+  theme(axis.title.y = element_blank(), #Get rid of y axis title
+        axis.title.x = element_blank(), #Get rid of x axis title, text, and ticks. Will combine with bacteria in cluster
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank())
 
 #Alternative with detailed_group which breaks cases down based on stool consistency
 percent_group_detailed <- sample_best_cluster_fit %>% 
@@ -117,39 +128,86 @@ percent_group_detailed <- sample_best_cluster_fit %>%
   ggplot()+
   geom_tile(aes(x=best_fitting_cluster, y=detailed_group, fill=percent_group))+
   theme_classic()+
-  scale_fill_distiller(palette = "YlGnBu", direction = 1)
+  scale_fill_viridis(name = "% Group")
+#  scale_fill_distiller(palette = "YlGnBu", direction = 1, name = "% Group")
 
 percent_cluster_detailed <- sample_best_cluster_fit %>% 
-  group_by(best_fitting_cluster, detailed_group) %>% 
+  group_by(cluster, detailed_group) %>% 
   summarize(group_cluster_total = n()) %>%
   #Make a new variable % group per cluster based on total cluster numbers
-  mutate(percent_cluster = case_when(best_fitting_cluster == "Partition_1" ~ (group_cluster_total/221)*100,
-                                     best_fitting_cluster == "Partition_10" ~ (group_cluster_total/421)*100,
-                                     best_fitting_cluster == "Partition_11" ~ (group_cluster_total/464)*100,
-                                     best_fitting_cluster == "Partition_12" ~ (group_cluster_total/81)*100,
-                                     best_fitting_cluster == "Partition_2" ~ (group_cluster_total/367)*100,
-                                     best_fitting_cluster == "Partition_3" ~ (group_cluster_total/283)*100,
-                                     best_fitting_cluster == "Partition_4" ~ (group_cluster_total/504)*100,
-                                     best_fitting_cluster == "Partition_5" ~ (group_cluster_total/243)*100,
-                                     best_fitting_cluster == "Partition_6" ~ (group_cluster_total/163)*100,
-                                     best_fitting_cluster == "Partition_7" ~ (group_cluster_total/421)*100,
-                                     best_fitting_cluster == "Partition_8" ~ (group_cluster_total/280)*100,
-                                     best_fitting_cluster == "Partition_9" ~ (group_cluster_total/484)*100,
+  mutate(percent_cluster = case_when(cluster == "1" ~ (group_cluster_total/221)*100,
+                                     cluster == "10" ~ (group_cluster_total/421)*100,
+                                     cluster == "11" ~ (group_cluster_total/464)*100,
+                                     cluster == "12" ~ (group_cluster_total/81)*100,
+                                     cluster == "2" ~ (group_cluster_total/367)*100,
+                                     cluster == "3" ~ (group_cluster_total/283)*100,
+                                     cluster == "4" ~ (group_cluster_total/504)*100,
+                                     cluster == "5" ~ (group_cluster_total/243)*100,
+                                     cluster == "6" ~ (group_cluster_total/163)*100,
+                                     cluster == "7" ~ (group_cluster_total/421)*100,
+                                     cluster == "8" ~ (group_cluster_total/280)*100,
+                                     cluster == "9" ~ (group_cluster_total/484)*100,
                                      TRUE ~ 0)) %>% #No samples should fall into this category
   mutate(detailed_group = fct_relevel(detailed_group, "diarrheal_control", "nondiarrheal_control", "nondiarrheal_case", "diarrheal_case", "unknown_case")) %>% #Specify the order of the groups
   ggplot()+
-  geom_tile(aes(x=best_fitting_cluster, y=detailed_group, fill=percent_cluster))+
+  geom_tile(aes(x=cluster, y=detailed_group, fill=percent_cluster))+
   theme_classic()+
-  scale_fill_distiller(palette = "YlGnBu", direction = 1)
+  scale_fill_viridis(name = "% Cluster")
+#  scale_fill_distiller(palette = "YlGnBu", direction = 1, name = "% Cluster")
 
+#Exploratory-figure out a better way to calculate percent group and percent cluster
 test <- sample_best_cluster_fit %>% 
   group_by(best_fitting_cluster, group) %>% 
   summarise(n=n()) %>% 
-  pivot_wider()
+  pivot_wider() %>% 
   mutate(percent_of_partition=n/sum(n)*100)
   
 test <- sample_best_cluster_fit %>% 
     group_by(best_fitting_cluster, group) %>% 
     summarise(n=n()) %>% 
-    pivot_wider()
+    pivot_wider() %>% 
   mutate(percent_of_group=n/sum(n)*100)
+  
+#Examine the bacteria that make up each community type:
+#Read in data that indicate the bacteria (at the genus level) membership of the different clusters
+community_otus <- read_tsv("data/process/cdi.subsample.genus.genus.dmm.mix.summary") %>% 
+  rename("otu" = "OTU") #rename to match otu column in taxonomy
+#Read in genus-level taxomy file and clean up genus names
+taxonomy <- read_tsv(file="data/process/cdi.genus.taxonomy") %>%
+  rename_all(tolower) %>% #remove uppercase from column names
+  rename(genus = taxonomy) %>% #Rename taxonomy to genus
+  # Clean up genus names  
+  mutate(genus=str_replace_all(genus, c('Bacteria_unclassified' = 'Unclassified',
+                                        "Clostridium_" = "Clostridium ", #Remove underscores after Clostridium
+                                        "_" = " ", #Removes all other underscores
+                                        "unclassified" = "unclassified")))
+#Join bacteria in clusters to taxonomy file
+bacteria_in_clusters <- community_otus %>% 
+  left_join(taxonomy, by = "otu") %>% #Join to taxonomy data frame by otu column in order to get the genus name for each OTU
+  select(genus,ends_with("mean")) %>% 
+  select(-P0.mean)
+bacteria_in_clusters_plot <- bacteria_in_clusters %>%   
+  slice_head(n=15) %>% 
+  add_row(bacteria_in_clusters %>% filter(genus == "Peptostreptococcaceae unclassified")) %>% #Also plot C. difficile OTU (confirm if this is the right one)
+  pivot_longer(-genus,names_to="cluster",values_to="relabund") %>% 
+  mutate(cluster=str_replace(cluster,"P(\\d*).mean","\\1")) %>% 
+  mutate(cluster= as.numeric(cluster)) %>% #Transform cluster variable type from character to numeric
+  ggplot() + geom_tile(aes(x=genus,y=cluster,fill=relabund))+
+  scale_y_continuous(breaks = c(1:12))+
+  labs(y = "Cluster")+
+  coord_flip()+
+  theme_classic()+
+  theme(axis.text.y = element_text(face = "italic"))+
+  scale_fill_distiller(palette = "YlGnBu", direction = 1, name = "Relative \nAbundance")+
+#  scale_fill_viridis(name = "Relative Abundance")
+  theme(axis.title.y = element_blank()) #Get rid of y axis title
+
+#Combine plot of percent of each group that belongs to each cluster and the relative abundances of bacteria in the clusters
+#To do: Figure out which OTUs corresond to C. difficile
+#Start by checking Otu 25 and Otu 85
+#Inspiration for combining plots: Kelly's Code Club: https://github.com/SchlossLab/plot-recreation/tree/key
+#Align heat amps
+combined_heatmaps <- align_plots(percent_cluster, bacteria_in_clusters_plot, align = 'v', axis = 'b') 
+#Combine heat maps
+plot_grid(combined_heatmaps[[1]], combined_heatmaps[[2]], rel_heights = c(1, 3), ncol = 1)
+
