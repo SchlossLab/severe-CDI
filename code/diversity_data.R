f24d4bee3c15fb689178162b8b90f7d97a1db61d
tomkoset <tomkoset@umich.edu>
2021-03-05 14:17:19 -0500

    Remove detailed group breakdown. Confirmed with clinicians. All cases passed stool consistency cutoff for C. diff testing by clinical micro lab

--- code/diversity_data.R
+++ code/diversity_data.R
@@ -108,49 +108,13 @@ shannon_plot <- plot_alpha_metric(shannon, "Shannon Diversity Index")
 invsimpson_plot <- plot_alpha_metric(invsimpson, "Inverse Simpson")
 richness_plot <- plot_alpha_metric(sobs, "Number of Observed OTUs")
 
-#Alternative alpha diversity plots with cases broken out based on stool consistency
-#Function to plot different alpha diversity metrics with the following arguments
-#alpha_metric: how alpha metric of choice is listed in dataframe. Ex. sobs, shannon, etc.
-#y_axis_label: how you want to label the alpha metric on the plot. Ex. "Shannon Diversity Index"
-plot_alpha_metric_detailed <- function(alpha_metric, y_axis_label){
-  diversity_data %>% 
-    group_by(detailed_group) %>% 
-    mutate(median = median({{ alpha_metric }})) %>% #Create column of median values for each group
-    ungroup() %>% 
-    ggplot(aes(x=detailed_group, y = {{ alpha_metric }}, color = detailed_group))+
-    geom_jitter(shape = 1, size=1, alpha = 0.5, show.legend = FALSE) +
-    geom_errorbar(aes(ymax= median, ymin= median), color = "black", size = 1)+#Add line to show median of each point
-    labs(title=NULL, 
-         x=NULL,
-         y=y_axis_label)+
-    scale_colour_manual(name=NULL,
-                        values=color_scheme_detailed,
-                        breaks=legend_groups_detailed,
-                        labels=legend_labels_detailed)+
-    scale_x_discrete(label = c("Diarrheal Case", "Non-Diarrheal Case", "Unknown Case", "Diarrheal Control", "Non-Diarrheal Control"))+
-    theme_classic()+
-    theme(legend.position = "bottom",
-          text = element_text(size = 19),# Change font size for entire plot
-          axis.text.x = element_text(angle = 45, hjust = 1), #Angle axis labels
-          axis.title.y = element_text(size = 17)) 
-}
-
-#Shannon, inverse simpson and richness plots 
-shannon_plot_detailed <- plot_alpha_metric_detailed(shannon, "Shannon Diversity Index")
-invsimpson_plot_detailed <- plot_alpha_metric_detailed(invsimpson, "Inverse Simpson")
-richness_plot_detailed <- plot_alpha_metric_detailed(sobs, "Number of Observed OTUs")
-
-plot_grid(invsimpson_plot, shannon_plot, richness_plot,
-          invsimpson_plot_detailed, shannon_plot_detailed, richness_plot_detailed,
-          nrow = 2)+
-  ggsave("results/figures/alpha_diversity.png", height = 10, width = 9.5)
+plot_grid(invsimpson_plot, shannon_plot, richness_plot, nrow = 1)+
+  ggsave("results/figures/alpha_diversity.png", height = 5, width = 9.5)
 #Inverse simpson
-plot_grid(invsimpson_plot, invsimpson_plot_detailed,
-          nrow = 1)+
+plot_grid(invsimpson_plot)+
   ggsave("results/figures/alpha_inv_simpson.png", height = 5, width = 4.5)
 #Richness
-plot_grid(richness_plot, richness_plot_detailed,
-          nrow = 1)+
+plot_grid(richness_plot)+
   ggsave("results/figures/alpha_richness.png", height = 5, width = 4.5)
 
 
