f4902354c6368dd9560f8d5fde4d553882bbfb58
tomkoset <tomkoset@umich.edu>
2021-03-12 18:00:46 -0500

    Create ml input data to classify IDSA severity status

--- code/mikropml_input_data.R
+++ code/mikropml_input_data.R
@@ -5,7 +5,6 @@ source("code/utilities.R") #Loads libraries, reads in metadata, functions
 otu_data <- read_tsv("data/mothur/cdi.opti_mcc.0.03.subsample.shared", col_types=cols(Group=col_character())) %>%
   select(-label, -numOtus) %>%
   rename(sample = Group) %>% #group is the same as sample in the metadata data frame
-  filter(!sample %in% contam_samples) %>%  #Remove 2 contaminated samples
   gather(-sample, key="otu", value="count") %>%
   mutate(rel_abund=count/5000) %>%  #Use 5000, because this is the subsampling parameter chosen.
   #using rel_abund also means the data will be normalized to between 0 and 1
@@ -13,8 +12,7 @@ otu_data <- read_tsv("data/mothur/cdi.opti_mcc.0.03.subsample.shared", col_types
   pivot_wider(id_cols = sample, names_from = otu, values_from = rel_abund) %>% #Transform dataframe so that each OTU is a different column
   left_join(select(metadata, group, sample), by = "sample") %>% 
   rename(outcome = group) %>%  #Rename group to outcome, this is what we will classify based on OTU relative abundances
-  relocate(outcome, before = sample) %>% 
-  select(-sample) #drop sample since we no longer need this column
+  relocate(outcome, before = sample) 
 
 #Make sure no controls are included in data (should be dropped since we subsampled to 5000 sequences)  
 otu_data %>% filter(outcome == "pbs_control")
@@ -25,6 +23,7 @@ otu_data %>% filter(outcome == "mock_control")
 #Write subsetted dataframe as .csv files to be used in the mikropml package
 subset_data <- function(data_frame, outcome1, outcome2, file_name){
   data_frame %>% 
+    select(-sample) #drop sample since we no longer need this column
     filter(outcome %in% c(outcome1, outcome2)) %>% 
     write_csv(path = paste0("data/process/ml_", file_name, ".csv"))
     
@@ -66,3 +65,10 @@ subset_data(genus_data, "case", "diarrheal_control", "genus_CvDC")
 #Create input data for diarrheal controls vs nondiarrheal controls
 subset_data(genus_data, "diarrheal_control", "nondiarrheal_control", "genus_DCvNDC")
 
+#Create mikropml data to classify IDSA severity status with OTU level data ----
+#Read in IDSA severity results & join to OTU data frame
+idsa_status <- read_csv("data/process/case_idsa_severity.csv") %>% 
+  left_join(otu_data, by = "sample") %>% #Join to otu_data
+  select(-sample, -outcome) %>% #Remove previous sample and outcome columns
+  rename(outcome = idsa_severity) %>% #Indicate idsa_severity column is the outcome of interest
+  write_csv(path = paste0("data/process/ml_idsa_severity.csv"))
