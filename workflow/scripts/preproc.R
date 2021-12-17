d0f3b78fa6d65c0ed27e1db6e87bf53fb190e544
Megan Coden <mlcoden@umich.edu>
2021-12-17 12:11:20 -0500

    updated lots of things

--- workflow/scripts/preproc.R
+++ workflow/scripts/preproc.R
@@ -1,8 +1,8 @@
-source("code/log_smk.R")
+source("workflow/scripts/log_smk.R")
 doFuture::registerDoFuture()
 future::plan(future::multicore, workers = snakemake@threads)
 
 data_raw <- readr::read_csv(snakemake@input[["csv"]])
-data_processed <- mikropml::preprocess_data(data_raw, outcome_colname = "outcome", method = NULL)
+data_processed <- mikropml::preprocess_data(data_raw, outcome_colname = snakemake@params[["outcome]]", method = snakemake@params[["method"]], seed = snakemake@params[["seed"]])
 
 saveRDS(data_processed, file = snakemake@output[["rds"]])
