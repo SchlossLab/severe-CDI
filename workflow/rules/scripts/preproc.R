3c054b4ba88ec74e90f6b23f1602c3174ace38a0
Megan Coden <mlcoden@umich.edu>
2022-01-07 14:37:59 -0500

    specified wildcard in preprocdata

--- workflow/rules/scripts/preproc.R
+++ workflow/rules/scripts/preproc.R
@@ -3,6 +3,6 @@ doFuture::registerDoFuture()
 future::plan(future::multicore, workers = snakemake@threads)
 
 data_raw <- readr::read_csv(snakemake@input[["csv"]])
-data_processed <- mikropml::preprocess_data(data_raw, outcome_colname = snakemake@params[["outcome"]])
+data_processed <- mikropml::preprocess_data(data_raw, outcome_colname = snakemake@wildcards[["outcome"]])
 
 saveRDS(data_processed, file = snakemake@output[["rds"]])
