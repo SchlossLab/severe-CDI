0230222218dc8d3a96ed2130e09132644f36cb4c
Megan Coden <mlcoden@umich.edu>
2022-01-29 13:25:56 -0500

    filter for 1 sample per patient

--- workflow/rules/scripts/prep_data_for_ml.R
+++ workflow/rules/scripts/prep_data_for_ml.R
@@ -20,9 +20,22 @@ multi_samples <- idsa_sra %>% group_by(patient_id) %>% filter(cdiff_case == "Cas
 # idsa_sra %>% filter(patient_id %in% multi_samples[["patient_id"]])
 #TODO: get one sample per patient
 sort_by_patient <-idsa_sra %>% group_by(patient_id)
-sort_by_patient[
+sorted <- sort_by_patient[
   with(sort_by_patient, order(patient_id, collection_date)),
 ]
+count <- 2
+my_entry <- sorted[1, 4]
+for (entry in sorted) {
+  if (sorted[count, 4] == my_entry) {
+    sorted <- sorted %>% slice(-c(count))
+    my_entry <- sorted[count, 4]
+  }
+  else {
+    my_entry <- sorted[count, 4]
+    ++count;
+  }
+}
+print(count)
 
 cases_severity_OTUs <- left_join(idsa_sra, dat_shared, by = "Group") %>% 
   filter(cdiff_case == 'Case', !is.na(idsa_severity)) %>% 
