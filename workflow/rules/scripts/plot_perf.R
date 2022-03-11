f6c245f4951f2e9255d174cefa900312181eaa12
Megan Coden <mlcoden@umich.edu>
2022-03-11 13:02:33 -0500

    added script to plot perf

--- workflow/rules/scripts/plot_perf.R
+++ workflow/rules/scripts/plot_perf.R
@@ -3,8 +3,25 @@ library(tidyverse)
 
 perf_plot <- snakemake@input[["csv"]] %>%
   read_csv() %>%
-  mikropml::plot_model_performance() +
-  theme_classic() +
-  scale_color_brewer(palette = "Dark2") +
-  coord_flip()
+    rename(
+        `train AUROC` = cv_metric_AUC,
+        `test AUROC` = AUC,
+        `test AUPRC` = prAUC
+    ) %>%
+    pivot_longer(c(`train AUROC`, `test AUROC`, `test AUPRC`),
+                 names_to = "metric"
+    ) %>%
+    mutate(metric = factor(metric,
+                           levels = c("test AUPRC", "test AUROC", "train AUROC")
+    )) %>%
+    ggplot(aes(x = value, y = metric)) +
+    geom_vline(xintercept = 0.5, linetype = "dashed") +
+    geom_boxplot() +
+    xlim(0.5, 1) +
+    labs(x = "Performance", y = "") +
+    theme_bw() +
+    theme(
+        plot.margin = unit(x = c(0, 5, 0, 0), units = "pt"),
+        axis.title.y = element_blank()
+    )
 ggsave(snakemake@output[["plot"]], plot = perf_plot)
