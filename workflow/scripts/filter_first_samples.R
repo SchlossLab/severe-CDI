8315b9cf7fbb7ca06e8995431337516cebd997d8
Kelly Sovacool <kellysovacool@gmail.com>
2022-04-26 17:40:40 -0400

    Fix assignment of all-cause severity

--- workflow/scripts/filter_first_samples.R
+++ workflow/scripts/filter_first_samples.R
@@ -1,11 +1,12 @@
 #' filter a dataframe to just one sample per patient, selecting the earliest sample
-filter_first_samples <- function(idsa_sra) {
+filter_first_samples <- function(metadat, 
+                                 patient_col = subject_id,
+                                 date_col = collection_date) {
   # modified from https://www.statology.org/select-first-row-in-group-dplyr/
-  idsa_sra %>% 
-    group_by(patient_id) %>% 
-    arrange(collection_date) %>% 
+  metadat %>% 
+    group_by({{ patient_col }}) %>% 
+    arrange({{ date_col }}) %>% 
     filter(row_number() == 1) %>% 
-    arrange(patient_id) %>% 
-    ungroup() %>% 
-    return()
+    arrange({{ patient_col }}) %>% 
+    ungroup()
 }
