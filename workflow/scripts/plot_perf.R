8a1f54333c682bd71f76433da91a530545674000
Kelly Sovacool <kellysovacool@gmail.com>
2022-05-23 13:58:19 -0400

    Facet by ml method

--- workflow/scripts/plot_perf.R
+++ workflow/scripts/plot_perf.R
@@ -18,7 +18,7 @@ perf_plot <- dat %>%
     geom_vline(xintercept = 0.5, linetype = "dashed") +
     geom_boxplot() +
     scale_color_brewer(palette = 'Dark2') +
-    #facet_wrap('metric', ncol = 1) +
+    facet_wrap('method', ncol = 1) +
     #xlim(0.5, 1) +
     labs(x = "Performance", y = "") +
     theme_bw() +
