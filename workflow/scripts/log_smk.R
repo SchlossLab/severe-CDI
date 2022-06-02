a5328053abadb18317e1a1c658821e5e7012fcfd
Kelly Sovacool <kellysovacool@gmail.com>
2022-06-02 14:54:20 -0400

    Allow script to run if snakemake DNE

--- workflow/scripts/log_smk.R
+++ workflow/scripts/log_smk.R
@@ -1,6 +1,8 @@
-if (!is.null(snakemake@log)) {
-  log_filepath <- snakemake@log[1][[1]]
-  log <- file(log_filepath, open = "wt")
-  sink(log, append = TRUE)
-  sink(log, append = TRUE, type = "message")
+if (exists('snakemake')) {
+  if (!is.null(snakemake@log)) {
+    log_filepath <- snakemake@log[1][[1]]
+    log <- file(log_filepath, open = "wt")
+    sink(log, append = TRUE)
+    sink(log, append = TRUE, type = "message")
+  }
 }
