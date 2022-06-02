338de66fd7d1b4437c2d5784263fbdf50cf639b3
Kelly Sovacool <kellysovacool@gmail.com>
2022-06-02 15:12:40 -0400

    Update performance figure for poster

--- workflow/scripts/plot_perf.R
+++ workflow/scripts/plot_perf.R
@@ -1,29 +1,32 @@
 source("workflow/scripts/log_smk.R")
 library(tidyverse)
-dat <- read_csv(snakemake@input[["csv"]])
+dat <- read_csv("results/performance_results_aggregated.csv")
 perf_plot <- dat %>%
-    rename(
-        `train AUROC` = cv_metric_AUC,
-        `test AUROC` = AUC,
-        `test AUPRC` = prAUC
-    ) %>%
-    pivot_longer(c(`train AUROC`, `test AUROC`, `test AUPRC`),
+    pivot_longer(-c(method, seed, outcome),
                  names_to = "metric"
-    ) %>%
-    mutate(metric = factor(metric,
-                           levels = c("train AUROC", "test AUROC", "test AUPRC")
-    )) %>%
-    filter(metric != "test AUPRC") %>% # TODO: plot PRC separately with different baseline
-    ggplot(aes(x = value, y = outcome, color = metric)) +
+                 ) %>%
+    mutate(data = case_when(metric == 'cv_metric_AUC' ~ 'train',
+                            metric == 'AUC' ~ 'test',
+                            TRUE ~ NA_character_),
+           outcome = case_when(outcome == 'idsa' ~ 'IDSA\n severity',
+                               outcome == 'attrib' ~ 'Attributable\n severity',
+                               outcome == 'allcause' ~ 'All-cause\n severity',
+                               TRUE ~ NA_character_)
+           ) %>%
+    filter(!is.na(data) & method == 'rf') %>% # TODO: plot PRC separately with different baseline
+    ggplot(aes(x = value, y = outcome, color = data)) +
     geom_vline(xintercept = 0.5, linetype = "dashed") +
     geom_boxplot() +
-    scale_color_brewer(palette = 'Dark2') +
-    facet_wrap('method', ncol = 1) +
+    scale_color_brewer(palette = 'Paired') +
+    #facet_wrap('method', ncol = 1) +
     #xlim(0.5, 1) +
-    labs(x = "Performance", y = "") +
+    labs(x = "AUROC") +
     theme_bw() +
     theme(
-        plot.margin = unit(x = c(0, 5, 0, 0), units = "pt"),
-        axis.title.y = element_blank()
+        plot.margin = unit(x = c(0, 0, 0, 0), units = "pt"),
+        axis.title.y = element_blank(),
+        legend.position = 'top',
+        legend.margin = margin(0, 0, 0, 0, unit = "pt")
     )
-ggsave(snakemake@output[["png"]], plot = perf_plot, device = "png")
+ggsave("figures/plot_perf.png", plot = perf_plot, device = "png", 
+       width = 5, height = 5)
