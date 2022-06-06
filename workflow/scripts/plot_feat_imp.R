c2e74ee0b67cd205dbf4366027457d5e0bbd8f4a
Kelly Sovacool <kellysovacool@gmail.com>
2022-06-06 19:01:22 -0400

    Color the dots for outcomes

--- workflow/scripts/plot_feat_imp.R
+++ workflow/scripts/plot_feat_imp.R
@@ -2,7 +2,10 @@ source("workflow/scripts/log_smk.R")
 library(cowplot)
 library(ggtext)
 library(glue)
+library(here)
 library(tidyverse)
+model_colors <- RColorBrewer::brewer.pal(3, 'Dark2')
+names(model_colors) <- c("idsa", 'attrib', 'allcause')
 
 feat_dat <- read_csv("results/feature-importance_results_aggregated.csv") %>% 
   rename(otu = names)
@@ -30,27 +33,30 @@ otu_sets <- data.frame(label_html = top_otus_order) %>%
          allcause = label_html %in% (top_otus_dat %>% filter(outcome == 'allcause') %>% pull(label_html))
          ) %>% 
   pivot_longer(c(idsa, attrib, allcause), names_to = "group", values_to = 'bool') %>% 
-  mutate(label_html = factor(label_html, levels = top_otus_order))
-
-model_colors <- RColorBrewer::brewer.pal(3, 'Dark2')
-names(model_colors) <- c("idsa", 'attrib', 'allcause')
-
-otu_sets_plot <- otu_sets %>% 
-  mutate(group_html = glue("<span style = 'color:{model_colors[group]};'>{group}</span>")) %>% 
+  mutate(label_html = factor(label_html, levels = top_otus_order),
+         group_html = glue("<span style = 'color:{model_colors[group]};'>{group}</span>"),
+         color_hex = case_when(bool == TRUE ~ model_colors[group],
+                               bool == FALSE ~ "white",
+                               TRUE ~ NA_character_)) %>% 
   mutate(group_html = fct_relevel(factor(group_html),
                                 "<span style = 'color:#1B9E77;'>idsa</span>", 
                                 "<span style = 'color:#D95F02;'>attrib</span>", 
-                                "<span style = 'color:#7570B3;'>allcause</span>")) %>%
+                                "<span style = 'color:#7570B3;'>allcause</span>"))
+dotplot_colors <- otu_sets %>% pull(color_hex) %>% unique()
+names(dotplot_colors) <- dotplot_colors
+
+otu_sets_plot <- otu_sets  %>%
   ggplot(aes(x = group_html, y = label_html)) +
   geom_tile(colour = "whitesmoke", size=0.5, fill = "white") +
-  geom_point(aes(color = bool), size = 4) + 
+  geom_point(aes(color = color_hex), size = 4) + 
   coord_fixed(ratio=1) +  
-  scale_color_manual(values = c("white", "black")) +
+  scale_color_manual(values = dotplot_colors) +
   scale_x_discrete(position = 'top') +
-  theme_minimal(base_size = 14) +
+  theme_minimal(base_size = 8) +
   theme(legend.position = "",
-        axis.text.y = element_markdown(),
-        axis.text.x.top = element_markdown(angle = 45, hjust = -0.1, vjust = -0.2),
+        axis.text.y = element_markdown(size = 8),
+        axis.text.x.top = element_markdown(size = 8, angle = 45, 
+                                           hjust = -0.1, vjust = -0.2),
         panel.grid = element_blank(),
         axis.ticks = element_blank(),
         axis.title = element_blank(),
@@ -72,21 +78,21 @@ feat_imp_plot <- dat %>%
     scale_color_manual(values = model_colors) +
     labs(title=NULL, 
          y=NULL,
-         x="Difference in AUROC")+
+         x="Difference in AUROC") +
     theme_bw() +
-    theme(text = element_text(size = 12),
+    theme(text = element_text(size = 8),
           axis.text.y = element_blank(), 
+          axis.text.x = element_text(size = 8),
           strip.background = element_blank(),
           legend.position = "none",
           panel.grid.major.y = element_blank(),
           plot.margin = unit(x = c(0, 0, 0, 0), units = "pt")) 
 
 combined_plot <- plot_grid(otu_sets_plot, feat_imp_plot, 
-                           align = 'h', nrow = 1, rel_widths = c(0.55, 0.45))
+                           align = 'h', nrow = 1, rel_widths = c(0.52, 0.48))
 ggsave(
     filename = here('figures', 'plot_feat_imp.png'), 
     plot = combined_plot,
     device = "png", dpi = 300, 
     units = "in", width = 5, height = 4
-)
-
+    )
