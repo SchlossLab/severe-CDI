b57f78874d86ddd5d6ab5803e36b3866a4c08a08
Kelly Sovacool <kellysovacool@gmail.com>
2022-11-05 18:51:28 -0400

    Add genus-level files

--- workflow/scripts/prep_genus_data_for_ml.R
+++ workflow/scripts/prep_genus_data_for_ml.R
@@ -4,9 +4,12 @@ library(schtools)
 
 log_snakemake(quiet = TRUE)
 
-shared <- read_tsv(snakemake@input[["shared"]])
-full_metadata <- read_csv(snakemake@input[["full_metadata"]]) %>% inner_join(shared)
-int_metadata <- read_csv(snakemake@input[["int_metadata"]]) %>% inner_join(shared)
+shared <- data.table::fread(snakemake@input[["shared"]]) %>% 
+  rename(sample_id = Group)
+full_metadata <- read_csv(snakemake@input[["full_metadata"]]) %>% 
+    inner_join(shared)
+int_metadata <- read_csv(snakemake@input[["int_metadata"]]) %>% 
+    inner_join(shared)
 
 full_metadata %>% select("idsa", starts_with("Otu")) %>% write_csv('data/process/idsa_full_genus.csv')
 int_metadata %>% select("idsa", starts_with("Otu")) %>% write_csv('data/process/idsa_int_genus.csv')
