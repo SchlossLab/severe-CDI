e82a1647c824bc384b78f859c6e8cc60df294b35
Kelly Sovacool <kellysovacool@gmail.com>
2022-11-05 20:10:14 -0400

    Use dev branch for schtools bug fix

--- workflow/scripts/get_genus_level.R
+++ workflow/scripts/get_genus_level.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(here)
+devtools::install_github('SchlossLab/schtools', ref="pool-counts-refactor")
 library(schtools)
 
 log_snakemake(quiet = TRUE)
@@ -9,3 +10,4 @@ otu_tax_dat <- read_tax(snakemake@input[["taxonomy"]])
 genus_list <- pool_taxon_counts(otu_shared_dat, otu_tax_dat, "genus")
 genus_list$shared %>% write_tsv(snakemake@output[["shared"]])
 genus_list$tax %>% write_tsv(snakemake@output[["taxonomy"]])
+
