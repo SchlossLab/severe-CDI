952aea5df6efcdc17fbb8b7095d157156f00aa81
Kelly Sovacool <kellysovacool@gmail.com>
2022-11-06 13:50:46 -0500

    log_snakemake() right away

--- workflow/scripts/get_genus_level.R
+++ workflow/scripts/get_genus_level.R
@@ -1,9 +1,9 @@
+schtools::log_snakemake(quiet = TRUE)
 library(tidyverse)
 library(here)
 devtools::install_github('SchlossLab/schtools', ref="pool-counts-refactor")
 library(schtools)
 
-log_snakemake(quiet = TRUE)
 
 otu_shared_dat <- data.table::fread(snakemake@input[["shared"]])
 otu_tax_dat <- read_tax(snakemake@input[["taxonomy"]])
