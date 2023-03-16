dfaba19acbc694c715cb0571e311888c9f184deb
Kelly Sovacool <kellysovacool@gmail.com>
2023-03-16 10:47:12 -0400

    Try dtplyr for memory efficiency

--- workflow/scripts/prep_pooled_data_for_ml.R
+++ workflow/scripts/prep_pooled_data_for_ml.R
@@ -1,12 +1,17 @@
+library(dtplyr)
 library(tidyverse)
 library(here)
 library(schtools)
 
 log_snakemake(quiet = TRUE)
 
+message('Reading shared file')
 shared <- data.table::fread(snakemake@input[["shared"]]) %>% 
   rename(sample_id = Group)
+message('Reading metadata and joining with shared file')
 read_csv(snakemake@input[["metadata"]]) %>% 
+  lazy_dt() %>% 
   inner_join(shared) %>% 
   select(snakemake@wildcards[['outcome']], starts_with("Otu")) %>% 
+  as_tibble() %>% 
   write_csv(snakemake@output[['csv']])
