0d3bfef7f8afff495b5561c4da548720962ee1e2
Kelly Sovacool <kellysovacool@gmail.com>
2023-03-17 16:18:09 -0400

    Select lines that start with ASV

--- workflow/scripts/prep_pooled_data_for_ml.R
+++ workflow/scripts/prep_pooled_data_for_ml.R
@@ -12,6 +12,6 @@ message('Reading metadata and joining with shared file')
 read_csv(snakemake@input[["metadata"]]) %>% 
   lazy_dt() %>% 
   inner_join(shared) %>% 
-  select(snakemake@wildcards[['outcome']], starts_with("Otu")) %>% 
+  select(snakemake@wildcards[['outcome']], starts_with("Otu"), starts_with("ASV")) %>% 
   as_tibble() %>% 
   write_csv(snakemake@output[['csv']])
