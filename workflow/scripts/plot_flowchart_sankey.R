4d7c3e5ff9a3c80fcea096628d3c0d7f96ebc938
Kelly Sovacool <kellysovacool@gmail.com>
2023-05-16 13:19:46 -0400

    Tweak legend

--- workflow/scripts/plot_flowchart_sankey.R
+++ workflow/scripts/plot_flowchart_sankey.R
@@ -10,11 +10,12 @@ metadat_cases <- read_csv('data/process/cases_full_metadata.csv') %>%
   filter(!(is.na(idsa) & is.na(allcause) & is.na(attrib) & is.na(pragmatic)))
 metadat_cases_sankey <- metadat_cases %>%
   mutate(
-    IDSA = if_else(is.na(idsa), 'missing_data', idsa),
-    `All-cause` = if_else(is.na(allcause), 'missing_data', allcause),
-    Attrib = if_else(is.na(attrib), 'missing_data', attrib),
-    Pragmatic = if_else(is.na(pragmatic), 'missing_data', pragmatic)
-  )
+    IDSA = if_else(is.na(idsa), 'Missing data', idsa),
+    `All-cause` = if_else(is.na(allcause), 'Missing data', allcause),
+    Attrib = if_else(is.na(attrib), 'Missing data', attrib),
+    Pragmatic = if_else(is.na(pragmatic), 'Missing data', pragmatic)
+  ) %>% 
+  mutate(across(c(IDSA, Attrib, `All-cause`, Pragmatic), ~ stringr::str_to_sentence(.x)))
 plot_sankey <- function(dat_long) {
   dat_long %>%
     rename(is_severe = node,
@@ -30,7 +31,8 @@ plot_sankey <- function(dat_long) {
       by = c('severity_definition', 'is_severe')
     ) %>% 
     mutate(severity_definition = factor(severity_definition,
-                                        levels = c('IDSA', 'All-cause', 'Attrib', 'Pragmatic'))) %>%
+                                        levels = c('IDSA', 'All-cause', 'Attrib', 'Pragmatic'))
+           ) %>%
     ggplot(aes(
       x = severity_definition,
       next_x = next_x,
@@ -41,20 +43,36 @@ plot_sankey <- function(dat_long) {
     )) +
     geom_sankey(flow.alpha = .6) +
     geom_sankey_label(color = 'white', show.legend = FALSE) +
-    scale_fill_manual(values = c(yes="#860967", no="#0F2A4B", 
-                                 'missing_data'="#BDBDBD")) +
+    scale_fill_manual(values = c(Yes="#860967", No="#0F2A4B", 
+                                 'Missing data'="#BDBDBD")) +
     labs(x = 'Severity Definition') +
     theme_sankey() +
     theme(text = element_text(size = 10, family = "Helvetica"))
 }
 sankey <- metadat_cases_sankey %>%
   make_long(c(IDSA, `All-cause`, Attrib, Pragmatic)) %>%
-  plot_sankey()
-flowchart <- ggdraw() + draw_image('figures/severity_flowchart.tiff')
-fig <- plot_grid(flowchart, sankey, 
-          nrow = 2, rel_heights = c(1, 0.5),
-          labels = 'AUTO')
+  plot_sankey() +
+  guides(fill=guide_legend(title="Is Severe",
+                           reverse = TRUE
+                           )) +
+  theme(legend.margin = margin(0,0,0,0),
+        plot.margin = margin(5,5,5,5))
+
+flowchart <- ggdraw() + 
+  draw_image('figures/severity_flowchart.tiff') +
+  theme(plot.margin = margin(5,5,5,5))
+
+fig <- plot_grid(
+  plot_grid(flowchart, NULL,
+            ncol = 2,
+            rel_widths = c(1, 0.2),
+            labels = c('A', '')),
+  sankey,
+  nrow = 2,
+  rel_heights = c(1, 0.7),
+  labels = c('', 'B')
+)
 ggsave('figures/flowchart_sankey.tiff', plot = fig,
-       device = 'tiff', bg = 'white',
-       width = 6.8, height = 7.5)
+       device = 'tiff', dpi = 600, bg = 'white',
+       width = 6, height = 6.5)
 
