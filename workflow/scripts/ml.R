d204017c0eeb044253e5f2e0d32fc5e936589b03
Kelly Sovacool <kellysovacool@gmail.com>
2023-05-28 20:09:59 -0400

    Remove balanced precision from ml training
    let's do it later during other performance calculations

--- workflow/scripts/ml.R
+++ workflow/scripts/ml.R
@@ -2,27 +2,15 @@ schtools::log_snakemake()
 library(here)
 library(mikropml)
 library(tidyverse)
-source(here('workflow','scripts','calc_balanced_precision.R'))
-add_cols <- function(dat) {
-  dat %>%
-    mutate(outcome = snakemake@wildcards[['outcome']],
-           taxlevel = snakemake@wildcards[['taxlevel']],
-           metric = snakemake@wildcards[['metric']],
-           dataset = snakemake@wildcards[['dataset']],
-           trainfrac = snakemake@wildcards[['trainfrac']])
-}
 
 doFuture::registerDoFuture()
 future::plan(future::multicore, workers = snakemake@threads)
 
+wildcards <- schtools::get_wildcards_tbl()
 outcome_colname <- snakemake@wildcards[['outcome']]
 ml_method <- snakemake@params[["method"]]
 seed <- as.numeric(snakemake@params[["seed"]])
 data_processed <- readRDS(snakemake@input[["rds"]])$dat_transformed 
-prior <- data_processed %>% 
-  calc_baseline_precision(outcome_colname = outcome_colname,
-                          pos_outcome = 'yes')
-message(paste('prior:', prior))
 
 ml_results <- run_ml(
   dataset = data_processed %>% 
@@ -38,16 +26,20 @@ ml_results <- run_ml(
   perf_metric_name = snakemake@wildcards[['metric']],
   perf_metric_function = caret::multiClassSummary
 )
-head(ml_results$performance)
+
+get_feature_importance(ml_results$trained_model,
+                       ml_results$test_data %>% as_tibble(),
+                       outcome_colname,
+                       perf_metric_fn = caret::multiClassSummary,
+                       perf_metric_name = snakemake@wildcards[['metric']],
+                       class_probs = TRUE,
+                       method = ml_method,
+                       seed = seed) %>%
+  left_join(wildcards, by = c("method", "seed")) %>%
+  write_csv(snakemake@output[["feat"]])
+
 ml_results$performance %>%
-  mutate(baseline_precision = prior,
-         balanced_precision = if_else(!is.na(Precision), 
-                                      calc_balanced_precision(Precision, prior), 
-                                      NA),
-         aubprc = if_else(!is.na(prAUC), 
-                          calc_balanced_precision(prAUC, prior), 
-                          NA)) %>% 
-  add_cols() %>%
+  left_join(wildcards, by = c("method", "seed")) %>%
   write_csv(snakemake@output[["perf"]])
 
 ml_results$test_data %>%
@@ -55,13 +47,3 @@ ml_results$test_data %>%
 
 saveRDS(ml_results$trained_model, file = snakemake@output[["model"]])
 
-get_feature_importance(ml_results$trained_model,
-                       ml_results$test_data %>% as_tibble(),
-                       outcome_colname,
-                       perf_metric_fn = caret::multiClassSummary,
-                       perf_metric_name = "AUC",
-                       class_probs = TRUE,
-                       method = ml_method,
-                       seed = seed) %>%
-  add_cols() %>%
-  write_csv(snakemake@output[["feat"]])
