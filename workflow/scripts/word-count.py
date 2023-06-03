d51ba234b9f7ab0cae3c53f2f3bed6fede1197d4
Kelly Sovacool <kellysovacool@gmail.com>
2023-06-03 13:37:04 -0400

    Count words for abstract & importance

---
+++ workflow/scripts/word-count.py
@@ -0,0 +1,20 @@
+import yaml
+
+with open('paper/paper.qmd', "r") as infile:
+    is_yaml = False
+    yaml_text = list()
+    for line in infile:
+        if line.startswith('---'):
+            is_yaml = not is_yaml
+        elif is_yaml:
+            yaml_text.append(line)
+
+yaml_dict = yaml.load(''.join(yaml_text), Loader=yaml.CLoader)
+
+with open('paper/word_count.txt', "w") as outfile:
+    for key in ['abstract', 'importance']:
+        if key in yaml_dict.keys():
+            word_count = len(yaml_dict[key].split())
+            wc_report = f"{key}: {word_count}\n"
+            print(wc_report)
+            outfile.write(wc_report)
