7841c888c16c4a1e65483f871d3cff2bed9c1004
Kelly Sovacool <kellysovacool@gmail.com>
2023-06-06 20:05:34 -0400

    Transpose count tables

--- workflow/scripts/sample_counts.R
+++ workflow/scripts/sample_counts.R
@@ -1,8 +1,8 @@
 schtools::log_snakemake()
 library(here)
 library(tidyverse)
-make_table <- function(dat) {
-  metadat_cases <- dat %>% 
+make_table <- function(metadat) {
+  metadat_cases <- metadat %>% 
     filter(!(is.na(idsa) & is.na(allcause) & is.na(attrib) & is.na(pragmatic)))
   
   counts <- metadat_cases %>%
@@ -22,21 +22,21 @@ make_table <- function(dat) {
     mutate(percent = round(n / total * 100, 1)) %>%
     filter(is_severe == 'yes') %>%
     select(severity_definition, total, percent)
-  percents <- totals %>% select(severity_definition, percent) %>%
-    pivot_wider(names_from = severity_definition, values_from = percent)%>%
-    mutate(stat = '% Severe', .before = allcause)
-  totals_percents <- totals %>%
-    select(-percent) %>%
-    pivot_wider(names_from = severity_definition, values_from = total) %>%
-    mutate(stat = 'n', .before = allcause) %>%
-    add_row(percents) %>%
-    select(stat, idsa, allcause, attrib, pragmatic)
-  return(totals_percents)
+  
+  return(
+    totals %>% 
+      rename(Severity = severity_definition,
+             n = total,
+             '% severe' = percent
+             ) %>% 
+      mutate(Severity = case_when(Severity == 'idsa' ~ 'IDSA',
+                                  Severity == 'allcause' ~ 'All-cause',
+                                  Severity == 'attrib' ~ 'Attributable',
+                                  Severity == 'pragmatic' ~ 'Pragmatic',
+                                  TRUE ~ NA_character_))
+  )
 }
-
 read_csv(here('data','process','cases_full_metadata.csv')) %>% 
-  make_table() %>% 
-  write_csv(here('results','count_table_full.csv'))
+  make_table() %>% write_csv(here('results','count_table_full.csv'))
 read_csv(here('data','process','cases_int_metadata.csv')) %>% 
-  make_table()  %>% 
-  write_csv(here('results','count_table_int.csv'))
+  make_table() %>% write_csv(here('results','count_table_int.csv'))
