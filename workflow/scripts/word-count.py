95b9cf832a786d2b808e3ecfdb2b6c41f34bfd4a
Kelly Sovacool <kellysovacool@gmail.com>
2023-06-06 20:05:19 -0400

    Improve body word count and use log

--- workflow/scripts/word-count.py
+++ workflow/scripts/word-count.py
@@ -1,5 +1,7 @@
 import yaml
 
+log_txt = list()
+
 def exclude_line(line):
     return line.startswith('<') or ('-->' in line)
 
@@ -19,24 +21,28 @@ with open(snakemake.input[0], "r") as infile:
         elif line.startswith('# Acknowledgements'):
             is_body = False
             break # end of body, stop parsing file
-        elif line.startswith('<div'):
-            is_body = False # toggle off for divs
-        elif line.startswith('# Abstract') or line.startswith('</div'):
-            is_body = True # toggle on for abstract and end of divs
+        elif line.startswith('<div') or (line.startswith('<!--') and not ('-->' in line)):
+            is_body = False # toggle off for divs and block comments
+        elif line.startswith('# Abstract') or line.startswith('</div') or ('-->' in line):
+            is_body = True # toggle on for abstract and ends of divs and comments
         elif is_body and not exclude_line(line):
-            #print('BODY\t', line)
+            log_txt.append(f'{line}')
             body_text += line.split()
 
 yaml_dict = yaml.load(''.join(yaml_text), Loader=yaml.CLoader)
 
+report = list()
+for key in ['abstract', 'importance']:
+    if key in yaml_dict.keys():
+        word_count = len(yaml_dict[key].split())
+        report.append(f"{key}: {word_count}\n")
+report.append(f"body: {len(body_text)}\n")
 
 with open(snakemake.output[0], "w") as outfile:
-    report = list()
-    for key in ['abstract', 'importance']:
-        if key in yaml_dict.keys():
-            word_count = len(yaml_dict[key].split())
-            report.append(f"{key}: {word_count}\n")
-    report.append(f"body: {len(body_text)}\n")
     outfile.writelines(report)
-    for rep in report:
-        print(rep)
+
+with open(snakemake.log[0], 'w') as logfile:
+    logfile.write("REPORT\n")
+    logfile.writelines(report)
+    logfile.write('\nBODY LINES\n')
+    logfile.writelines(log_txt)
